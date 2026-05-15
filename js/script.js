e0ded6d7d7d65700f70950340150fd24268932e5
dudupizzoli <edupizzolijr@gmail.com>
2026-05-15 11:40:13 -0300

    feat : manipulação de elementos, exemplos de funções e janelas modais com dialog

--- js/script.js
+++ js/script.js
@@ -301,7 +301,20 @@ botaoEntrar.addEventListener("click", (e)=>{
         
         for (const u of usuarios) {
             if(u.email === dadosForm.email && u.senha === dadosForm.senha){
-                alert("Login realizado com sucesso!");
+                
+              //capturar a janela de dialog:
+              const modal = document.querySelector("#meuModal");
+              modal.showModal();
+
+              //Capturando o botão do dialog para fechar a janela
+              const botaoModal = document.querySelector("#btnFecharModal");
+              //Atrelar um evento para o botão de fechamento encerrar a janela de dialog
+              botaoModal.addEventListener("click", ()=>{
+                //utilizando o elemento dialog já capturado para encerrar.
+                modal.close();
+              });
+
+
                 isValid = true;
                 break;
             }
