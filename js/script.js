36c72718992c8d82cc4fad7e36aa666595f84b53
heniqueosuka <henriqueosuka@gmail.com>
2026-05-15 12:10:24 -0300

    feat: alguma coisa ai

--- js/script.js
+++ js/script.js
@@ -288,19 +288,58 @@ botaoEntrar.addEventListener("click", (e)=>{
 
     //Iniciando a validação de fato, colocando a lista de usuários contra o objeto dadosForm:
 
+    let isValid = false;
+
     if(usuarios.length > 0){
 
-        usuarios.forEach((u)=>{
+        for (const u of usuarios) {
             if(u.email === dadosForm.email && u.senha === dadosForm.senha){
-                alert("Login realizado com sucesso!");
-            }   
-    });
-    
+               
+                //Capturar a janela de dialog:
+                const modal = document.querySelector("#meuModal");
+                modal.showModal();
+
+                //Capturando o botao do dialog para fechar a janela.
+                const botaoModal = document.querySelector("#btnFecharModal");
+                //Atrelar um evento para o botão de fechamento encerrar a janela de dialog.
+                botaoModal.addEventListener("click", ()=>{
+                  //utilizando o elemento dialog já capturado para encerrar.
+                  modal.close();
+                });
+
+                //TIMER COM INJEÇÃO DA MSG DE SUCESSO
+                //Capturar o elemento que apresenta a msg no dialog
+                const divMsg = document.querySelector("#msg");
+
+                //Adicionando uma nova tag a esta div capturada com a propriedade innerHTML.
+
+                divMsg.innerHTML = "<p>Login realizado com SUCESSO!</p><p>Você será redirecionado em 5 segundos...</p>";
+
+                let contador = 5;
+
+                const intervalo = setInterval( ()=>{
+                    contador--;
+                    divMsg.innerHTML = `<p>Login realizado com SUCESSO!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+
+                    if(contador === 0){
+                      clearInterval(intervalo);
+                    }
+                }, 1000 );
+                
+                isValid = true;
+                break;
+            }
+        }
+ 
     }else{
         alert("Ocorreu um problema com as informações do sistema!");
     }
 
-
-
+    if (!isValid) {
+        alert("Email ou senha incorretos!");
+    }
 });
 
+//Pra casa
+// Reaproveitar a janela de MODAL no caso de o usuário errar o login.
+// Realizar o redirect com window em caso de sucesso!
