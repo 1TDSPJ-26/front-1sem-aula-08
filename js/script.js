fd2e03428153faf6c5b8e111d8d6d70e37c03c0f
lucasvieira-tech <martivieiralucas@gmail.com>
2026-05-15 12:10:05 -0300

    feat:Realizando apresentação de temporizador em janela de MODAL usando seinterval Exercício para casa.

--- js/script.js
+++ js/script.js
@@ -308,7 +308,24 @@ botaoEntrar.addEventListener("click", (e)=>{
                 modal.closest();
             });
 
-            
+             //TIMER COM INJEÇÃO DA MSG DE SUCESO
+             //capturar o elemento que apresenta a msg no dialog
+             const divMsg = document.querySelector("#msg");
+
+             //Adicionando uma nova tag a esta div capturada com a propriedade innerHTML.
+                divMsg.innerHTML = "<p>Login realizado com SUCESSO!</p><p>Você sera redirecionado em 5 segundos...</p>";
+                
+                let contador = 5;
+
+                const intervalo = setInterval( ()=>{ 
+                contador--;
+                    divMsg.innerHTML = `<p>Login realizado com SUCESSO!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+
+                    if(contador === 0) {
+                        clearInterval(intervalo);
+                    }
+                }, 1000 );
+
                 isValid = true;
                 break;
             }
@@ -324,8 +341,5 @@ botaoEntrar.addEventListener("click", (e)=>{
 });
 
 //Pra casa
-// Incrementar a validação com uma mensagem temporizadora na tela:
-// Utilize a função setInterval().
-// Onde o usuário recebe a mensagem de Sucesso e que vai ser redirecionado em 5 segundos,
-// com um contador rodando.
-// Utilize a função window.location.href = destino para redirecionar o usuário!
+Reaproveitar a janela de MODAL no caso de o usuário errar o login.
+Realizar o redirect com window em caso de sucesso!
